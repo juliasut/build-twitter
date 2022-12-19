00953234cc8d5d3128ff67e671a313ac739589e8
juliasut <juliasutygina@gmail.com>
2022-12-18 17:31:24 -0700

    Init setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import AuthApp from "./AuthApp";
+import { useAuthUser } from "./context/auth-context";
+import UnAuthApp from "./UnAuthApp";
+
+export default function App() {
+
+  const authUser = useAuthUser()
+
+  if (authUser) {
+    return <AuthApp/>
+  } else { return <UnAuthApp/>}
+}
